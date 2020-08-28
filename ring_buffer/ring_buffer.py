6d1c58267948f3ab58b424f22a0f8fbccf820d50
Greg <remph819@gmail.com>
2020-08-28 16:48:33 -0700

    finished ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,24 @@
+from collections import deque
+
+
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.data = []
+        self.storage = []
+        self.cur = 0
 
     def append(self, item):
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
 
-
+        else:
+            self.storage[self.cur] = item
+            self.cur += 1
+            if self.cur == self.capacity:
+                self.cur = 0
 
     def get(self):
-        pass
+        return self.storage
+
+
+
